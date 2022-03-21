d04718169b47ab1f2e675c7d1ba4cd3dcb16daf1
evde-saglik <evde-saglik@DESKTOP-JP90FOO.mshome.net>
2022-03-21 17:03:32 +0300

    ...........

--- city_api/src/main/java/com/util/city_api/controller/AuthController.java
+++ city_api/src/main/java/com/util/city_api/controller/AuthController.java
@@ -1,5 +1,40 @@
 package com.util.city_api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.util.city_api.core.constant.CoreEnumExceptionMessages;
+import com.util.city_api.core.exception.exceptionModels.UnSuccessfulException;
+import com.util.city_api.product_core.dto._coreDto.UserDto;
+import com.util.city_api.product_core.request.createRequest.UserCreateRequest;
+import com.util.city_api.service._abstract.IUserService;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
-}
+	private final IUserService userService;
+	
+	public AuthController(IUserService userService) {
+		this.userService = userService;
+	}
+
+
+	@PostMapping("/signUp")
+	    public ResponseEntity<?> signup(UserCreateRequest userCreateRequest) {
+              
+		   UserDto userDto = userService.createUser(userCreateRequest);
+		   
+		   	if(userDto!=null)
+		   	{
+		   		return ResponseEntity.ok(userDto);
+		   	}
+		   	else {
+		   		
+		   		throw new UnSuccessfulException(CoreEnumExceptionMessages.UN_SUCCESSFUL_CREATED_USER,"user did not cerated beacause : ");
+		   	}
+	   
+	    	
+	    }}
