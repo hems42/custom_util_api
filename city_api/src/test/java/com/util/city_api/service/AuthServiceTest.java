a5a13d667fe584d6fcf575be72d990911fa2785a
evde-saglik <evde-saglik@DESKTOP-JP90FOO.mshome.net>
2022-04-01 16:04:18 +0300

    ...

--- city_api/src/test/java/com/util/city_api/service/AuthServiceTest.java
+++ city_api/src/test/java/com/util/city_api/service/AuthServiceTest.java
@@ -1,11 +1,88 @@
 package com.util.city_api.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.util.city_api.service._abstract.IAuthService;
+import com.util.city_api.service.concrete.AuthService;
 
 public class AuthServiceTest {
+	
+	IAuthService authService;
 
 	@BeforeEach
 	void setup() {
 		
+		authService = new AuthService();
 	}
+	
+	
+
+	
+   	   //singup metod logic...
+	/*	 
+	 * request ok ??
+	 * user created allready by email or username ??
+	 * refresh token create ?? 
+	 * accesstoken create ??
+	 * confirm token create ??
+	 * email send ??
+	 * 
+	 * NOTE : user device platfrom checking and modifying user creating...
+	 */
+	
+	
+	
+	     // signup metod :
+	@Test
+	void WhenUserAllReadyCreatedByUsernameThenItMustThro__Exception() {}
+
+
+	@Test
+	void t1() {}
+	
+	@Test
+	void t2() {}
+	
+	@Test
+	void t3() {}
+	
+	@Test
+	void t4() {}
+	
+	@Test
+	void t5() {}
+	
+	@Test
+	void t6() {}
+	
+	@Test
+	void t7() {}
+	
+	@Test
+	void t8() {}
+	
+	@Test
+	void t9() {}
+	
+	@Test
+	void t10() {}
+	
+	@Test
+	void t11() {}
+	
+	@Test
+	void t12() {}
+	
+	@Test
+	void t13() {}
+	
+	@Test
+	void t14() {}
+	
+	@Test
+	void t15() {}
+	
+	@Test
+	void t16() {}
 }
